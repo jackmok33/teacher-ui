573c107ed2d9756b2844362b5bd91dab2403d32c
jackmok33 <jackmok33@gmail.com>
2025-03-21 15:43:38 +0000

    added a guide on home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import  { useState } from "react";
 import { useNavigate } from "react-router-dom"; // Import useNavigate for routing
 import "../styles/Homepage.css";
+import teacherFlow from "../assets/teacherFlow.png"; // Import the image
 
 function Homepage() {
     const [gameMode, setGameMode] = useState("json"); // State for selecting the game mode
@@ -33,6 +34,23 @@ function Homepage() {
                         Get Started
                     </button>
                 </form>
+                <br/>
+                <br/>
+                <h2>Step Guide</h2>
+                <img src={teacherFlow} alt="Teacher UI Flow" style={{width:"700px"}}></img>
+                <div className="textbody">
+                    <h3>Step 1 - Generate Questions</h3>
+                    <p>User can decide generating questions by i) uploading JSON file or ii) Using Object Recognition to recognise object as prompts for AI generated Questions</p>
+                </div>
+                <div className="textbody">
+                    <h3>Step 2 - Generate Background Image</h3>
+                    <p>User then can customise the prompts for generating the background image for the game board and the quiz background</p>
+                </div>
+                <div className="textbody">
+                    <h3>Step 3 - Game Config</h3>
+                    <p>User then can finalise the game configuration and generate room code for student to join</p>
+                </div>
+                
             </div>
         </div>
     );
